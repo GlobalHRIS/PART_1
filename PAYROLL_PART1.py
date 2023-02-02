11c36258a9aed32571958678686243093bdf7473
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-02 15:55:34 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -9,16 +9,21 @@ def read_data(file_name):
     return df
 
 def main():
+# Streamklit User Interface part
     st.set_page_config(page_title="GlobalHRIS", page_icon=":guardsman:", layout="wide")
     st.image("logo.png", width=400)
     st.title("Global HR Implementation Services Limited \n Employee Data Search")
     emp_number = st.text_input("Enter Employee Number")
-   
+ # Reading the February employee data  
     df1 = read_data("Feb_Data.csv")
-  
     if emp_number:
         Feb_emp_data = df1[df1['Employee Number'] == int(emp_number)]
         st.write(Feb_emp_data)
+ # Reading the March employee data
+     df2 = read_data("March_Data.csv")
+    if emp_number:
+        Mar_emp_data = df2[df2['Employee Number'] == int(emp_number)]
+        st.write(Mar_emp_data)
    
       
 if __name__ == '__main__':
