14ac4f78ed0ffac1d583b10238db5d1b9ae35ec1
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-02 15:39:06 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -12,16 +12,14 @@ def main():
     st.set_page_config(page_title="GlobalHRIS", page_icon=":guardsman:", layout="wide")
     st.image("logo.png", width=400)
     st.title("Global HR Implementation Services Limited \n Employee Data Search")
-
-
+    
     df1 = read_data("Feb_Data.csv")
-    df2 = read_data("March_Data.csv")
-
     emp_number = st.text_input("Enter Employee Number")
     if emp_number:
         Feb_emp_data = df1[df1['Employee Number'] == int(emp_number)]
-        Mar_emp_data = df2[df2['Employee Number'] == int(emp_number)]
         st.write(Feb_emp_data)
+        df2 = read_data("March_Data.csv")   
+        Mar_emp_data = df2[df2['Employee Number'] == int(emp_number)] 
         st.write(Mar_emp_data) 
         st.write("Total Net pay: Rs.{Net Pay:.2f} ")
       
