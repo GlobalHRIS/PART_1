c3487738124a70108995b31a7fd6a602ff8aec74
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-02 15:45:40 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -18,6 +18,7 @@ def main():
     if emp_number:
         Feb_emp_data = df1[df1['Employee Number'] == int(emp_number)]
         st.write(Feb_emp_data)
+    elif:    
         df2 = read_data("March_Data.csv")   
         Mar_emp_data = df2[df2['Employee Number'] == int(emp_number)] 
         st.write(Mar_emp_data) 
