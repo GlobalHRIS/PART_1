1ce5b1effbe647085f04ea098fdabf3894c2c83f
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-03 12:00:38 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -14,15 +14,37 @@ st.set_page_config(page_title="GlobalHRIS", page_icon=":guardsman:", layout="wid
 st.image("logo.png", width=400)
 st.title("Global HR Implementation Services Limited \n Employee Data Search")
 emp_number = st.text_input("Enter Employee Number")
+
+
+def calculate_change(previous_net_pay, current_net_pay):
+    return current_net_pay - previous_net_pay
+
+# Load the employee data from two CSV files
+employee_data_1 = pd.read_csv('Feb_Data.csv')
+employee_data_2 = pd.read_csv('March_Data.csv')
+
+
+# Combine the two dataframes into one
+employee_data = pd.concat([employee_data_1, employee_data_2], ignore_index=True)
+if emp_number:
+    # Calculate the net pay for each month
+    employee_data['Net Pay'] = employee_data.apply(lambda row: calculate_net_pay(row['Salary'], row['Deductions']), axis=1)
+
+    # Calculate the change in net pay between each month
+    employee_data['Change in Net Pay'] = employee_data['Net Pay'].diff()
+
+    # Display the updated dataframe
+    st.write(employee_data)
+
 # Reading the February employee data  
-df1 = read_data("Feb_Data.csv")
-if emp_number:
-    Feb_emp_data = df1[df1['Employee Number'] == int(emp_number)]
-    st.write(Feb_emp_data)
+#df1 = read_data("Feb_Data.csv")
+#if emp_number:
+    #Feb_emp_data = df1[df1['Employee Number'] == int(emp_number)]
+   # st.write(Feb_emp_data)
 # Reading the March employee data
-df2 = read_data("March_Data.csv")
-if emp_number:
-    Mar_emp_data = df2[df2['Employee Number'] == int(emp_number)]
-    st.write(Mar_emp_data)
+#df2 = read_data("March_Data.csv")
+#if emp_number:
+    #Mar_emp_data = df2[df2['Employee Number'] == int(emp_number)]
+    #st.write(Mar_emp_data)
    
 
