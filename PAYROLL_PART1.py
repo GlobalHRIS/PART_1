386bc1eed1ad8c86ba26d198c9891d9f8f984802
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-06 10:47:30 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -11,37 +11,16 @@ st.set_page_config(page_title="GlobalHRIS", page_icon=":guardsman:", layout="wid
 st.image("logo.png", width=400)
 st.title("Global HR Implementation Services Limited \n Employee Data Search")
 emp_number = st.text_input("Enter Employee Number")
-# Reading the February employee data  
 
-# Read CSV files from List
+# Reading the employee data 
 df = pd.concat(map(pd.read_csv, ['Feb_Data.csv', 'March_Data.csv']))
 if emp_number:
     emp_data = df[df['Employee Number'] == int(emp_number)]
     st.write(emp_data)
-    
-import itertools
-
-d = {}
-
-for fi, f in enumerate(df):
-    data = open(df)
-    for line in data:
-        sl = line.split()
-        name = sl[0]
-        val = int(sl[1])
-        if name not in d:
-            d[name] = {}
-        if fi not in d[name]:
-            d[name][fi] = []
-        d[name][fi].append(val)
-    fh.close()
-
-for name, vals in d.items():
-    if len(vals) == len(files):
-        for var in itertools.product(*vals.values()):
-            if max(var) - min(var) <= 20:
-                out = '{}\t{}'.format(name, "\t".join(map(str, var)))
-                st.write(out)
-                break
+  
+df1=pd.read_csv("Feb_Data.csv",usecols=["Net pay Feb"])
+df2=pd.read_csv("March_Data.csv",usecols=["Net pay Mar"])
+netpay_differnce = df1-df2
+st.write(netpay_differnce)
    
 
