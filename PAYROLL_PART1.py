4384e364b0737ae6a359265f0395b2e740e22364
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-07 17:56:06 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -6,10 +6,8 @@ def read_data(file_name):
     return df
 
 # Reading the employee data 
-#df1 = pd.read_csv('Feb_Data.csv')
-#usecols = ['Employee Number','First Name','Last Name','Net Pay'])
-df2 = pd.read_csv('March_Data.csv')
-                  #usecols = ['Employee Number','First Name','Last Name','Net Pay'])
+df1 = pd.read_csv('feb_emp_data.csv'), usecols = ['Employee Number','First Name','Last Name','Net Pay'])
+df2 = pd.read_csv('march_emp__Data.csv'), usecols = ['Employee Number','First Name','Last Name','Net Pay'])
 
 # Streamlit User Interface part
 st.set_page_config(page_title="GlobalHRIS", page_icon=":guardsman:", layout="wide")
@@ -21,12 +19,12 @@ emp_number = st.text_input("Enter Employee Number")
 #df = pd.concat(map(pd.read_csv, ['Feb_Data.csv','March_Data.csv']))
 # Condition checking              
 if emp_number:
-    #feb_emp_data = df1[df1['Employee Number'] == int(emp_number)]
-    Mar_emp_data = df2[df2['Employee Number'] == int(emp_number)]
+    feb = df1[df1['Employee Number'] == int(emp_number)]
+    Mar = df2[df2['Employee Number'] == int(emp_number)]
     #st.title("The Employee data for february")
-    st.write(feb_emp_data)
+    st.write(feb)
     #st.title("The Employee data for March")
-    st.write(Mar_emp_data)
+    st.write(Mar)
     
     #netpay_differnce = df1-df2
     #st.write('netpay differnce of the employee is:'netpay_differnce )
