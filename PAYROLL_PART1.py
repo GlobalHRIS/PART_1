1e4937b75e8268a52173e1ef6b86948d17e64e33
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-07 17:59:03 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -1,18 +1,16 @@
 import streamlit as st
 import pandas as pd
 
-
-
-# Reading the employee data 
-df1 = pd.read_csv('feb_emp_data.csv', usecols = ['Employee Number','First Name','Last Name','Net Pay'])
-df2 = pd.read_csv('march_emp_data.csv', usecols = ['Employee Number','First Name','Last Name','Net Pay'])
-
 # Streamlit User Interface part
 @st.cache
 st.set_page_config(page_title="GlobalHRIS", page_icon=":guardsman:", layout="wide")
 st.image("logo.png", width = 400)
 st.title("Global HR Implementation Services Limited \n Employee Data Search")
-emp_number = st.text_input("Enter Employee Number") 
+emp_number = st.text_input("Enter Employee Number")
+
+# Reading the employee data 
+df1 = pd.read_csv('feb_emp_data.csv', usecols = ['Employee Number','First Name','Last Name','Net Pay'])
+df2 = pd.read_csv('march_emp_data.csv', usecols = ['Employee Number','First Name','Last Name','Net Pay'])
 
 
 #df = pd.concat(map(pd.read_csv, ['Feb_Data.csv','March_Data.csv']))
