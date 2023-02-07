98817359ee3a7fe43e7e47119bc07a93c24324bb
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-07 11:46:12 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -13,9 +13,10 @@ st.title("Global HR Implementation Services Limited \n Employee Data Search")
 emp_number = st.text_input("Enter Employee Number")
 
 # Reading the employee data 
-df = pd.concat(map(pd.read_csv['Feb_Data.csv'df1 = pd.read_csv('Feb_Data.csv', usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay']),
-'March_Data.csv',usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
+df1 = pd.read_csv('Feb_Data.csv', usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
+df2 = pd.read_csv('March_Data.csv',usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
 
+df = pd.concat(map(df1,df2)
 if emp_number:
     emp_data = df[df['Employee Number'] == int(emp_number)]
     st.write(emp_data)
