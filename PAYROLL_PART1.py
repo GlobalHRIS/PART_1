e8b999c7d66ad99f64529b124d91f8e6634d4da1
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-07 15:14:30 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -7,8 +7,8 @@ def read_data(file_name):
     return df
 
 # Reading the employee data 
-#df1 = pd.read_csv('Feb_Data.csv', usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
-#df2 = pd.read_csv('March_Data.csv',usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
+df1 = pd.read_csv('Feb_Data.csv', usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
+df2 = pd.read_csv('March_Data.csv',usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
 
     
 # Streamlit User Interface part
@@ -17,7 +17,7 @@ st.image("logo.png", width=400)
 st.title("Global HR Implementation Services Limited \n Employee Data Search")
 emp_number = st.text_input("Enter Employee Number") 
 
-df = pd.concat(map(read_csv('March_Data.csv','Feb_Data.csv')))
+df = pd.concat(df1,df2)
                
 if emp_number:
     emp_data = df[df['Employee Number'] == int(emp_number)]
