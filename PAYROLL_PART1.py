e619236eb9811e6427254315809d38822864cb54
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-07 15:00:39 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -5,18 +5,20 @@ import pandas as pd
 def read_data(file_name):
     df = pd.read_csv(file_name)
     return df
-    
-# Streamlit User Interface part
-st.set_page_config(page_title="GlobalHRIS", page_icon=":guardsman:", layout="wide")
-st.image("logo.png", width=400)
-st.title("Global HR Implementation Services Limited \n Employee Data Search")
-emp_number = st.text_input("Enter Employee Number")
 
 # Reading the employee data 
 df1 = pd.read_csv('Feb_Data.csv', usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
 df2 = pd.read_csv('March_Data.csv',usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
 
-df = pd.concat(map(df1,df2)
+    
+# Streamlit User Interface part
+st.set_page_config(page_title="GlobalHRIS", page_icon=":guardsman:", layout="wide")
+st.image("logo.png", width=400)
+st.title("Global HR Implementation Services Limited \n Employee Data Search")
+emp_number = st.text_input("Enter Employee Number") 
+
+df = pd.concat(map('March_Data.csv', 'Feb_Data.csv'))
+               
 if emp_number:
     emp_data = df[df['Employee Number'] == int(emp_number)]
     st.write(emp_data)
