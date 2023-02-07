8bdc897cc8116208091654fa97dce280baf0decd
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-07 16:01:17 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -7,8 +7,8 @@ def read_data(file_name):
     return df
 
 # Reading the employee data 
-#df1 = pd.read_csv('Feb_Data.csv', usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
-#df2 = pd.read_csv('March_Data.csv',usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
+df1 = pd.read_csv('Feb_Data.csv', usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
+df2 = pd.read_csv('March_Data.csv',usecols = ['Employee Number', 'Forename', 'Surname', 'Net Pay'])
 
 # Streamlit User Interface part
 st.set_page_config(page_title="GlobalHRIS", page_icon=":guardsman:", layout="wide")
@@ -16,11 +16,13 @@ st.image("logo.png", width=400)
 st.title("Global HR Implementation Services Limited \n Employee Data Search")
 emp_number = st.text_input("Enter Employee Number") 
 
-df = pd.merge(map(pd.read_csv, ['Feb_Data.csv','March_Data.csv']))
+#df = pd.concat(map(pd.read_csv, ['Feb_Data.csv','March_Data.csv']))
               
 if emp_number:
-    emp_data = df[df['Employee Number'] == int(emp_number)]
-    st.write(emp_data)
+    feb_emp_data = df1[df1['Employee Number'] == int(emp_number)]
+    Mar_emp_data = df2[df2['Employee Number'] == int(emp_number)]
+    st.write(feb_emp_data)
+    st.write(Mar_emp_data)
     
     
     
