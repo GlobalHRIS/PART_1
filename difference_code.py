6d9292aa2edc2607cd74a7501f17a4c67d43805e
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-08 14:58:37 +0000

    Add files via upload

---
+++ difference_code.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  8 13:21:37 2023
+
+@author: 46058007
+"""
+
+#import streamlit as st
+import pandas as pd
+
+# Streamlit User Interface part
+#st.set_page_config(page_title ="GlobalHRIS", page_icon =":guardsman:", layout ="wide")
+#st.image("logo.png", width = 400)
+#st.title("Global HR Implementation Services Limited \n Employee Data Search")
+
+
+df1 =  pd.read_csv('netpaydata.csv')
+
+print(df1.head(n=5))
+
+
+
+emp_number = input("Enter Employee Number")
+
+# Condition checking  
+            
+
+    
+for emp_number in df1:
+  if(df1['Employee_Number'] == emp_number):
+      df1['diff_value']= df1['Net pay Feb'] - df1['Net pay March']
+      print(diff_value)
+ 
+
+
+ 
