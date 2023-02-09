3723c9bc21ffc51876eccd4cc37e55c3d632182d
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-09 09:51:14 +0000

    Update PAYROLL_PART1.py

--- PAYROLL_PART1.py
+++ PAYROLL_PART1.py
@@ -12,19 +12,19 @@ df2 = pd.read_csv('March_Data.csv',usecols = ['Employee Number', 'Forename', 'Su
 
 #df = pd.concat(map(pd.read_csv, ['Feb_Data.csv','March_Data.csv']))
 
-def netpay_diff(emp_number):    
-    netpay_diff = [df1["Net Pay"]-df2["Net Pay"]]
-    st.write(netpay_diff)
-    return netpay_diff
+#def netpay_diff(emp_number):    
+    #netpay_diff = df1["Net Pay"]-df2["Net Pay"]
+    #st.write(netpay_diff)
+    #return netpay_diff
 
 # Condition checking              
 if emp_number:
     feb = df1[df1['Employee Number'] == int(emp_number)]
     Mar = df2[df2['Employee Number'] == int(emp_number)]
-    st.title("##The Employee data for February##")
+    st.write("""### February month data""")
     st.write(feb)
-    st.title("##The Employee data for March##")
+    st.title("""###March Month data""")
     st.write(Mar)
-    netpay_diff(emp_number)
-    
+    netpay_diff = df1["Net Pay"]-df2["Net Pay"]
+
 
