ccf785ea7f7da6d2c88fbcde454c4cada152a16b
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-09 10:04:37 +0000

    Update difference_code.py

--- difference_code.py
+++ difference_code.py
@@ -9,15 +9,12 @@ st.title("Global HR Implementation Services Limited \n Net Pay Difference Calcul
 df1 =  pd.read_csv('netpaydata.csv')
 emp_number = st.text_input("Enter Employee Number")
 
-
-
 # Condition checking    
 #ok = st.button("Calculate Netpay Difference")
 if emp_number:
     #Calculate the Net Pay difference
-    diff = df1['Net Pay Feb']- df1['Net pay March']
-    st.write(diff)
-    st.subheader(f"The netpay diffeernce is ${diff[0]:.2f}")
+    st.write(netpay_diff)
+    st.subheader(f"The netpay diffeernce is ${netpay_diff[0]:.2f}")
 
 
  
