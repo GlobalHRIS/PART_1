c5f7947a421aa76739f7187811892e574b19a6c4
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-13 16:48:30 +0000

    Update difference_code.py

--- difference_code.py
+++ difference_code.py
@@ -6,17 +6,16 @@ st.image("logo.png", width = 400)
 st.title("Global HR Implementation Services Limited \n AI Employee Net Pay Difference Calculator")
 emp_number = st.text_input("Enter Employee Number")
 
-def calculate_difference():
-    # Load the employee data from a CSV file into a Pandas DataFrame
-    df = pd.read_csv('netpaydata.csv')
-    
+# Load the employee data from a CSV file into a Pandas DataFrame
+df = pd.read_csv('netpaydata.csv')
+
+def calculate_difference()
     # Calculate the net pay difference between February and March
     if emp_number:
+        d = df['Employee_number'] == int(emp_number)]
         df['Net Pay Difference'] = df['Net Pay March'] - df['Net Pay Feb']
-        # Display the result
-        st.write("The net pay difference for employee number {} is: {}".format(emp_number, Net Pay Difference)) 
-    
-
+        st.write("The net pay difference for employee number {} is: ".format(emp_number, Net Pay Difference)) 
+    else:
 # Call the calculate_difference function
 calculate_difference()
 
