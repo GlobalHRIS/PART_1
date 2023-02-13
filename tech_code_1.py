0335862b0e6ab80c03232dd143edd31d91b97216
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-13 14:59:37 +0000

    Update tech_code_1.py

--- tech_code_1.py
+++ tech_code_1.py
@@ -3,8 +3,7 @@ import csv
 
 def calculate_difference(employee_number):
     # Open the CSV file containing employee data
-    with open('netpaydata.csv', 'r') as file:
-        reader = csv.DictReader(file)
+        reader = pd.read_csv(netpaydata.csv,encoding='cp1252')
         
         # Loop through each row in the CSV file
         for row in reader:
