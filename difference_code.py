8b58bf3059c45449918ff04de2af35bd823c39bb
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-13 18:22:28 +0000

    Update difference_code.py

--- difference_code.py
+++ difference_code.py
@@ -1,26 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-st.set_page_config(page_title ="GlobalHRIS", page_icon =":guardsman:", layout ="wide")
-st.image("logo.png", width = 400)
-st.title("Global HR Implementation Services Limited \n Net Pay Difference Calculator")
-emp_number = st.text_input("Enter Employee Number")
-
 # Load the employee data from a CSV file into a Pandas DataFrame
 df = pd.read_csv('netpay_data.csv')
-
-
 for row in df:
      if emp_number:
           netpay = df[df['Employee Number'] == int(emp_number)]
           st.write("Employee Data Found")
           st.write(netpay)
-          current_month_salary = df['Net Pay March']
-          previous_month_salary = df['Net Pay Feb']
+          current_month_salary = int(df['Net Pay March'])
+          previous_month_salary = int(df['Net Pay Feb'])
           difference = current_month_salary - previous_month_salary
           st.write("The net pay difference for employee number {} is:".format(employee_number), difference)
           
-          
+st.set_page_config(page_title ="GlobalHRIS", page_icon =":guardsman:", layout ="wide")
+st.image("logo.png", width = 400)
+st.title("Global HR Implementation Services Limited \n Net Pay Difference Calculator")
+emp_number = st.text_input("Enter Employee Number")       
           
           #current_month_salary = int(row['Net Pay March'])
           #previous_month_salary = int(row['Net Pay Feb'])
