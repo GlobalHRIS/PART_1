bf8747aa928bc9afe478b24655f22aefce58d8af
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-13 23:50:47 +0000

    Update difference_code.py

--- difference_code.py
+++ difference_code.py
@@ -18,8 +18,9 @@ column1 = st.selectbox("Select the current month netpay", df.columns)
 column2 = st.selectbox("Select the previous month netpay", df.columns)
     
 new_column_name = st.text_input("Enter the name of the new column", "netpaydifference")
-    
-for row in df
+
+emp_number = st.text_input("Enter Employee Number") 
+for row in df:
         if emp_number:
                 empdata = df[df['Employee Number'] == int(emp_number)]
                 df[netpaydifference] = df[Net Pay March] - df[Net Pay Feb]
