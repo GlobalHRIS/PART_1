4834d7c4ef342fee0a89039784cdc85cbb4f4e65
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-13 15:25:08 +0000

    Update tech_code_1.py

--- tech_code_1.py
+++ tech_code_1.py
@@ -23,7 +23,7 @@ for row in df1:
         return
         
  # Calculate the difference
- difference = current_month_salary - previous_month_salar
+ difference = current_month_salary - previous_month_salary
  # Display the result
  st.write("The net pay difference for employee number {} is:".format(employee_number), difference)
 
