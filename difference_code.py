6eb1f4897c53b4212bfcff8b9296fb2b21a1f797
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-13 18:13:28 +0000

    Update difference_code.py

--- difference_code.py
+++ difference_code.py
@@ -15,7 +15,7 @@ for row in df:
           netpay = df[df['Employee Number'] == int(emp_number)]
           st.write("Employee Data Found")
           st.write(netpay)
-          difference= df['Net Pay March'] - df['Net Pay Feb']
+          difference= df[ df['Net Pay March'] - df['Net Pay Feb']]
           st.write("The net pay difference for employee number {} is:".format(employee_number), difference)
           
           
