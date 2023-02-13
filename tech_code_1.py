2217e4a0735c2c3b22b5ae943ba5fa07a8bf994c
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-13 15:03:16 +0000

    Update tech_code_1.py

--- tech_code_1.py
+++ tech_code_1.py
@@ -7,7 +7,7 @@ def calculate_difference(employee_number):
         
         # Loop through each row in the CSV file
     for row in reader:
-        if row['Employee_number'] == int(employee_number):
+        if row['Employee_number'] == str(employee_number):
              current_month_salary = int(row['Net Pay March'])
              previous_month_salary = int(row['Net Pay Feb'])
              break
