c5a4c8a5a0e162f9dec3ebcf1f2fb5bea1e860be
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-13 21:54:32 +0000

    Update difference_code.py

--- difference_code.py
+++ difference_code.py
@@ -10,7 +10,7 @@ emp_number = st.text_input("Enter Employee Number")
 df = pd.read_csv('netpay_data.csv')
 for row in df:
      if emp_number:
-          netpay = df[df['Employee Number'] == int(emp_number)]
+          netpay = df[df['Employee Number'] == str(emp_number)]
           st.write("Employee Data Found")
           st.write(netpay)
           current_month_salary = int(row['Net Pay March'])
