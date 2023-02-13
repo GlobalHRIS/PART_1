dbf4111a68a140ff44ce89c1080b24cbdbe82865
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-13 19:13:11 +0000

    Update difference_code.py

--- difference_code.py
+++ difference_code.py
@@ -13,5 +13,6 @@ for row in df:
           netpay = df[df['Employee Number'] == int(emp_number)]
           st.write("Employee Data Found")
           st.write(netpay)
-          st.write("The net pay difference for employee number {} is:".format(emp_number),Netpay_Diff)   
+          difference = df['Netpay_Diff']
+          st.write("The net pay difference for employee number {} is:".format(emp_number),difference)   
                
