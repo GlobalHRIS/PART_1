964c5a840ee54ffb32cffae600ea5f4cc60d0555
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-14 00:27:47 +0000

    Update difference_code.py

--- difference_code.py
+++ difference_code.py
@@ -11,8 +11,8 @@ uploaded_file = st.file_uploader("Choose a csv file", type=["csv"])
 
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
-    column1 = st.selectbox("Select the first column", df.columns)
-    column2 = st.selectbox("Select the second column", df.columns)
+    column1 = st.selectbox("Select the first month", df.columns)
+    column2 = st.selectbox("Select the second month", df.columns)
     new_column_name = st.text_input("Enter the name of the new column", "Net Pay Difference")
     df[new_column_name] = df[column1] - df[column2]
     #st.write("Net Pay Difference of all the Employees")
