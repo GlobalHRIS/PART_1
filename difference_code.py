b81c45997d86829182042fe66f04659dca1758e9
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-15 12:39:47 +0000

    Update difference_code.py

--- difference_code.py
+++ difference_code.py
@@ -16,57 +16,39 @@ st.title("Upload the input file to check the difference")
 st.write("Upload an image, PDF, or CSV file below:")
 
 # Create file uploader for image, PDF, and CSV files
-file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png", "pdf", "csv"])
+file = st.file_uploader("Choose a file", type=["jpg", "jpeg", "png"] "pdf", "csv"])
 
-# If a file is uploaded, process the file
-if file is not None:
-    # If file is a PDF, display the first page of the PDF
-    if file.type == 'application/pdf':
-        pdf = pdftotext.PDF(file)
-        first_page = pdf[0]
-        st.write(first_page)
+ 
+# Uploading the input csv file
 
-    # If file is a CSV, display the data as a table
-    elif file.type == 'text/csv':
-        df = pd.read_csv(file)
-        column1 = st.selectbox("Select the first month", df.columns)
-        column2 = st.selectbox("Select the second month", df.columns)
-        new_column_name = st.text_input("Enter the name of the new column", "Net Pay Difference")
-        df[new_column_name] = df[column1] - df[column2]
-        st.write("Net Pay Difference of all the Employees")
-        st.write(df)
-        emp_number = st.text_input("Enter the employee number:")
-        for row in df:
-           if emp_number:
+st.write("Please upload a csv file")
+uploaded_csvfile = st.file_uploader("Choose a csv file", type=["csv"])
+
+if uploaded_csvfile is not None:
+    df = pd.read_csv(uploaded_file)
+    column1 = st.selectbox("Select the first month", df.columns)
+    column2 = st.selectbox("Select the second month", df.columns)
+    new_column_name = st.text_input("Enter the name of the new column", "Net Pay Difference")
+    df[new_column_name] = df[column1] - df[column2]
+    st.write("Net Pay Difference of all the Employees")
+    st.write(df)
+    emp_number = st.text_input("Enter the employee number:")
+    for row in df:
+        if emp_number:
                  empdata = df[df['Employee Number'] == int(emp_number)]
                  st.write("The net pay difference for employee number {} is:".format(emp_number))
                  st.write(empdata)   
                  break
                     
- 
+# uploading input file as payslip pdf
 
-# Uploading the input file
-
-#st.write("Please upload a csv file")
-#uploaded_file = st.file_uploader("Choose a csv file", type=["csv"])
-
-#if uploaded_file is not None:
-   # df = pd.read_csv(uploaded_file)
-    #column1 = st.selectbox("Select the first month", df.columns)
-    #column2 = st.selectbox("Select the second month", df.columns)
-   # new_column_name = st.text_input("Enter the name of the new column", "Net Pay Difference")
-   # df[new_column_name] = df[column1] - df[column2]
-    #st.write("Net Pay Difference of all the Employees")
-    #st.write(df)
-    #emp_number = st.text_input("Enter the employee number:")
-    #for row in df:
-       # if emp_number:
-                # empdata = df[df['Employee Number'] == int(emp_number)]
-                 #st.write("The net pay difference for employee number {} is:".format(emp_number))
-                 #st.write(empdata)   
-                 #break
-                    
- 
+st.write("Please upload a pdf file")
+uploaded_pdffile = st.file_uploader("Choose a pdf file", type=["pdf"])
+if uploaded_pdffile is not None:
+    pdf = pdftotext.PDF(file)
+        first_page = pdf[0]
+        st.write(first_page)
+        break
      
 
        
