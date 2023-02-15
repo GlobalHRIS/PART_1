4e76b0bc2cd7198c0a3de819e218c485c9c69bcd
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-15 13:02:25 +0000

    Update fileupload.py

--- fileupload.py
+++ fileupload.py
@@ -64,16 +64,16 @@ def main():
 				column1 = st.selectbox("Select the first month",df.columns)
 				column2 = st.selectbox("Select the second month",df.columns)
 				new_column_name = st.text_input("Enter the name of the new column", "Net Pay Difference")
-    				df[new_column_name] = df[column1] - df[column2]
-    				st.write("Net Pay Difference of all the Employees")
-    				st.write(df)
-    				emp_number = st.text_input("Enter the employee number:")
-    				for row in df:
-        				if emp_number:
-                 				empdata = df[df['Employee Number'] == int(emp_number)]
-                 				st.write("The net pay difference for employee number {} is:".format(emp_number))
-                 				st.write(empdata)   
-                 				break
+				df[new_column_name] = df[column1] - df[column2]
+				st.write("Net Pay Difference of all the Employees")
+				st.write(df)
+				emp_number = st.text_input("Enter the employee number:")
+				for row in df:
+					if emp_number:
+						empdata = df[df['Employee Number'] == int(emp_number)]
+						st.write("The net pay difference for employee number {} is:".format(emp_number))
+						st.write(empdata)                    			
+						break
                     
 
 
