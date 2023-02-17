86db00d08958525fff0da07c4d547c0f92bd8ca5
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-17 20:24:15 +0000

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -1,47 +1,38 @@
 import streamlit as st
 import pandas as pd
 import mysql.connector
-from mysql.connector import Error
 
-# Function to establish a connection with the MySQL database
-def create_connection():
-    try:
-        conn = mysql.connector.connect(
-            host='localhost',
-            user='root',
-            password='LeakTimeBike4242',
-            database='globalhris'
-        )
-        if conn.is_connected():
-            print('Connected to MySQL database')
-            return conn
+# Connect to the MySQL database
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="LeakTimeBike4242",
+    database="globalhris"
+)
 
-    except Error as e:
-        print(e)
+# Create a cursor object to execute SQL queries
+cursor = db.cursor()
 
-# Streamlit app
-def app():
-    st.title("Load data into MySQL database")
+# Streamlit interface to upload the CSV file
+st.title("Upload a CSV file to MySQL")
+file = st.file_uploader("Choose a CSV file", type="csv")
 
-    # Create file uploader
-    uploaded_file = st.file_uploader("Choose a CSV file to upload", type="csv")
+if file is not None:
+    # Read the contents of the CSV file into a Pandas DataFrame
+    df = pd.read_csv(file)
 
-    if uploaded_file is not None:
-        # Read uploaded CSV file
-        df = pd.read_csv(uploaded_file)
+    # Create a MySQL table with the same column names as the DataFrame
+    columns = ", ".join(df.columns)
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS netpay_data ({columns})")
 
-        # Create connection to MySQL database
-        conn = create_connection()
-        cursor = conn.cursor()
+    # Insert the rows of the DataFrame into the MySQL table
+    for i, row in df.iterrows():
+        values = tuple(row)
+        placeholders = ", ".join(["%s"] * len(row))
+        query = f"INSERT INTO netpay_data ({columns}) VALUES ({placeholders})"
+        cursor.execute(query, values)
 
-        # Loop through each row in the DataFrame and insert into MySQL database
-        for i, row in df.iterrows():
-            insert_query = "INSERT INTO emp1 (column1, column2, column3) VALUES (%s, %s, %s)"
-            values = (row['column1'], row['column2'], row['column3'])
-            cursor.execute(insert_query, values)
-            conn.commit()
+    # Commit the changes to the MySQL database
+    db.commit()
 
-        st.write("Data loaded successfully into MySQL database")
-
-if __name__ == '__main__':
-    app()
+    st.write("CSV file uploaded and loaded into MySQL database!")
