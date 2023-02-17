a405fdfdf5926c84edef3739307d65ee0addff78
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-17 20:09:18 +0000

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -1,45 +1,46 @@
 import streamlit as st
-import pandas as pd
-from sqlalchemy import create_engine
+import mysql.connector
+from mysql.connector import Error
 
-# Create a function to load the data into a database
-def load_data_to_database(dataframe, db_name, table_name):
-    # Create a connection to the database
-    engine = create_engine(f'sqlite:///{db_name}.db', echo=False)
-    
-    # Write the data to the database
-    dataframe.to_sql(table_name, con=engine, if_exists='replace', index=False)
+# Function to establish a connection with the MySQL database
+def create_connection():
+    try:
+        conn = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='LeakTimeBike4242',
+            database='globalhris'
+        )
+        if conn.is_connected():
+            print('Connected to MySQL database')
+            return conn
 
-# Create a Streamlit app
-def main():
-    # Set the title and the header
-    st.title('Data Upload and Database Load')
-    st.header('Upload your data and load it into a database')
+    except Error as e:
+        print(e)
 
-    # Create a file uploader
-    uploaded_file = st.file_uploader('Choose a CSV file', type=['csv'])
+# Streamlit app
+def app():
+    st.title("Load data into MySQL database")
 
-    # If a file was uploaded
+    # Create file uploader
+    uploaded_file = st.file_uploader("Choose a CSV file to upload", type="csv")
+
     if uploaded_file is not None:
-        # Read the file into a DataFrame
-        data = pd.read_csv(uploaded_file)
+        # Read uploaded CSV file
+        df = pd.read_csv(uploaded_file)
 
-        # Show the DataFrame in the app
-        st.write('Original Data', data)
+        # Create connection to MySQL database
+        conn = create_connection()
+        cursor = conn.cursor()
 
-        # Get the name of the database and table
-        db_name = st.text_input('Database name', 'my_database')
-        table_name = st.text_input('Table name', 'my_table')
+        # Loop through each row in the DataFrame and insert into MySQL database
+        for i, row in df.iterrows():
+            insert_query = "INSERT INTO your_table_name (column1, column2, column3) VALUES (%s, %s, %s)"
+            values = (row['column1'], row['column2'], row['column3'])
+            cursor.execute(insert_query, values)
+            conn.commit()
 
-        # If the user entered a name for the database and table
-        if db_name and table_name:
-            # Load the data into the database
-            load_data_to_database(data, db_name, table_name)
-            st.write('Data loaded into database')
-        else:
-            st.write('Please enter a database name and table name')
-    else:
-        st.write('Please upload a CSV file')
+        st.write("Data loaded successfully into MySQL database")
 
 if __name__ == '__main__':
-    main()
+    app()
