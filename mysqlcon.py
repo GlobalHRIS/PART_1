ad4ab50b7c14c8bca81ff8b9a93f1ee35d9689de
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-17 22:57:26 +0000

    Update mysqlcon.py

--- mysqlcon.py
+++ mysqlcon.py
@@ -2,37 +2,34 @@ import streamlit as st
 import pandas as pd
 import mysql.connector
 
-# Connect to the MySQL database
-db = mysql.connector.connect(
-    host="localhost:3306",
-    user="root",
-    password="LeakTimeBike4242",
-    database="globalhris"
+# Set up MySQL connection
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="LeakTimeBike4242",
+  database="globalhris"
 )
 
-# Create a cursor object to execute SQL queries
-cursor = db.cursor()
+# Define function to insert data into MySQL table
+def insert_data(table_name, data):
+    cursor = mydb.cursor()
+    cols = "`,`".join([str(i) for i in data.columns.tolist()])
+    for i,row in data.iterrows():
+        sql = "INSERT INTO " + table_name + " (`" +cols + "`) VALUES (" + "%s,"*(len(row)-1) + "%s)"
+        cursor.execute(sql, tuple(row))
+        mydb.commit()
 
-# Streamlit interface to upload the CSV file
-st.title("Upload a CSV file to MySQL")
-file = st.file_uploader("Choose a CSV file", type="csv", accept_multiple_files=False)
+# Streamlit app
+st.title("Upload CSV to MySQL")
 
+# Get user input
+file = st.file_uploader("Choose a CSV file", type="csv")
+table_name = st.text_input("Enter table name")
+
+# Process file and insert data into MySQL
 if file is not None:
-    # Read the contents of the CSV file into a Pandas DataFrame
-    df = pd.read_csv(file)
-
-    # Create a MySQL table with the same column names as the DataFrame
-    columns = ", ".join(df.columns)
-    cursor.execute(f"CREATE TABLE IF NOT EXISTS my_table ({columns})")
-
-    # Insert the rows of the DataFrame into the MySQL table
-    for i, row in df.iterrows():
-        values = tuple(row)
-        placeholders = ", ".join(["%s"] * len(row))
-        query = f"INSERT INTO my_table ({columns}) VALUES ({placeholders})"
-        cursor.execute(query, values)
-
-    # Commit the changes to the MySQL database
-    db.commit()
-
-    st.write("CSV file uploaded and loaded into MySQL database!")
+    data = pd.read_csv(file)
+    st.write(data)
+    if st.button("Insert into MySQL"):
+        insert_data(table_name, data)
+        st.success("Data inserted into MySQL!")
