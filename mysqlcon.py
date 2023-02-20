2aca77a72f9d9c06362a9f27204d40ff143ce2ec
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-20 10:57:14 +0000

    Update mysqlcon.py

--- mysqlcon.py
+++ mysqlcon.py
@@ -7,7 +7,7 @@ def create_connection():
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
-        password="",
+        password="LeakTimeBike4242",
         database="globalhris"
     )
     return conn
