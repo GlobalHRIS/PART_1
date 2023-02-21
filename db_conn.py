e1a68cc492514ede19640368b1d801067c3b7368
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-21 09:21:46 +0000

    Update db_conn.py

--- db_conn.py
+++ db_conn.py
@@ -33,8 +33,7 @@ def main():
         	month2 = st.selectbox("Select the second month", df.columns) 
         	Net_Pay_Diff = st.text_input("Enter the name of the new column", "Net Pay Difference")
         	df[Net_Pay_Diff] = df[month1] - df[month2]
-		emp_number = st.text_input("Enter the Employee Number:")
-
+		emp_number = st.text_input('Enter the Employee Number:')
 		for row in df:
 			if emp_number:
 				empdata = df[df['Employee Number'] == int(emp_number)]
