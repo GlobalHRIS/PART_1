06f9de876c439c9f94c0b72343701af2085dedac
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-21 12:52:47 +0000

    Update pdftocsv.py

--- pdftocsv.py
+++ pdftocsv.py
@@ -1,29 +1,35 @@
 import streamlit as st
-import PyPDF2
-import csv
+import tabula
+import pandas as pd
 
-def extract_text(pdf_file):
-    with pdf_file:
-        read_pdf = PyPDF2.PdfFileReader(pdf_file)
-        text = []
-        for page in range(read_pdf.getNumPages()):
-            page_obj = read_pdf.getPage(page)
-            text.append(page_obj.extractText())
-        return '\n'.join(text)
+# Set page title
+st.set_page_config(page_title="PDF to CSV Converter")
 
-def convert_to_csv(pdf_file):
-    text = extract_text(pdf_file)
-    csv_data = [line.split(",") for line in text.split("\n")]
-    with open("output.csv", "w", newline="") as csv_file:
-        writer = csv.writer(csv_file)
-        for row in csv_data:
-            writer.writerow(row)
+# Define function to convert PDF to CSV
+def convert_pdf_to_csv(pdf_file):
+    # Use tabula to read table data from PDF
+    df = tabula.read_pdf(pdf_file, pages='all')
+    # Concatenate tables if multiple tables in one page
+    df = pd.concat(df)
+    # Convert dataframe to CSV format
+    csv = df.to_csv(index=False)
+    return csv
 
-st.title("PDF to CSV Converter")
-st.write("Upload a PDF file to convert to CSV.")
+# Define Streamlit app
+def app():
+    # Set app title
+    st.title("PDF to CSV Converter")
 
-uploaded_file = st.file_uploader("Choose a PDF file", type="pdf")
+    # Upload PDF file
+    st.header("Upload PDF file")
+    pdf_file = st.file_uploader("Choose a PDF file", type="pdf")
 
-if uploaded_file is not None:
-    convert_to_csv(uploaded_file)
-    st.download_button(label="Download CSV", data="output.csv", file_name="output.csv")
+    # Convert PDF to CSV and download CSV file
+    if pdf_file is not None:
+        st.header("Convert to CSV and download")
+        csv = convert_pdf_to_csv(pdf_file)
+        st.download_button(label="Download CSV", data=csv, file_name="converted.csv", mime="text/csv")
+
+# Run Streamlit app
+if __name__ == "__main__":
+    app()
