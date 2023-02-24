3b2c8102d87833cb734c671f46e317f58ac090be
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-02-24 09:52:21 +0000

    Update pdftocsv.py

--- pdftocsv.py
+++ pdftocsv.py
@@ -5,31 +5,22 @@ import pandas as pd
 # Set page title
 st.set_page_config(page_title="PDF to CSV Converter")
 
-# Define function to convert PDF to CSV
-def convert_pdf_to_csv(pdf_file):
-    # Use tabula to read table data from PDF
-    df = tabula.read_pdf(pdf_file, pages='all')
-    # Concatenate tables if multiple tables in one page
-    df = pd.concat(df)
-    # Convert dataframe to CSV format
-    csv = df.to_csv(index=False)
-    return csv
+# Set page header
+st.header("PDF to CSV Converter")
 
-# Define Streamlit app
-def app():
-    # Set app title
-    st.title("PDF to CSV Converter")
+# Upload PDF file
+pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 
-    # Upload PDF file
-    st.header("Upload PDF file")
-    pdf_file = st.file_uploader("Choose a PDF file", type="pdf")
+# Check if file is uploaded
+if pdf_file is not None:
+    # Read PDF file and extract tables using tabula-py
+    df = tabula.read_pdf(pdf_file)
 
-    # Convert PDF to CSV and download CSV file
-    if pdf_file is not None:
-        st.header("Convert to CSV and download")
-        csv = convert_pdf_to_csv(pdf_file)
-        st.download_button(label="Download CSV", data=csv, file_name="converted.csv", mime="text/csv")
+    # Concatenate all tables into a single dataframe
+    df_concat = pd.concat(df)
 
-# Run Streamlit app
-if __name__ == "__main__":
-    app()
+    # Save the dataframe as a CSV file
+    csv_file = df_concat.to_csv(index=False)
+
+    # Display the CSV file as a download link
+    st.download_button("Download CSV", data=csv_file, file_name="output.csv")
