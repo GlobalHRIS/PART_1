576751700784b49360a66e4bf9fadbd41b8e3926
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-03-03 13:24:19 +0000

    Update pdftocsv.py

--- pdftocsv.py
+++ pdftocsv.py
@@ -72,9 +72,11 @@ def main():
 							if csv_filename:
 								# Save the CSV file to the user's computer
 								csv_file = pdf_data.to_csv(f"{csv_filename}.csv", index=False)
-								if st.download_button('Download file', data)
-								#st.success("CSV file saved!")
+								st.download_button('Download file', data)
+								if st.download_button
 									st.write(data)
+									break
+							#st.success("CSV file saved!")
 							#csv_file = st.text_input("Enter CSV filename:")
 							#new_file = split.to_csv(split, index=False)
 							#myfile = pd.read_csv(new_file)
