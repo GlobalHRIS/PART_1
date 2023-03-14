59e668ec3e2a206619c286af7a2b966e5fc4e7ba
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-03-14 11:19:21 +0000

    Update pdftocsv.py

--- pdftocsv.py
+++ pdftocsv.py
@@ -97,7 +97,7 @@ def main():
 					st.write(empdata)
 					break
 					
-	elif choice =="AI Decision Tool"
+	elif choice =="AI Decision Tool":
 		st.subheader("AI Solution Provider")
 		file = st.file_uploader("Upload File",type=["csv"])
 		if file is not None:
