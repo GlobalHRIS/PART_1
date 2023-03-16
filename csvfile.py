dfc82aaaa2c52061d37b8afdaf3b0a57165df473
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-03-16 22:36:05 +0000

    Update csvfile.py

--- csvfile.py
+++ csvfile.py
@@ -14,7 +14,13 @@ file = st.file_uploader("Upload a text file", type=["txt"])
 
 if file:
     st.write(f"Uploading file: {file.name}")
-    text_to_csv(file)
+    #text_to_csv(file)
+    data = []
+    with open(file) as f:
+        for line in f.readlines():
+            data.append(line.strip().split())
+    df = pd.DataFrame(data)
+    df.to_csv('output.csv', index=False)
     st.download_button(
         label="Download CSV",
         data=open('output.csv', 'rb').read(),
