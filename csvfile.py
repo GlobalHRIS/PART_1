330ea0c05de036f55843c1e01e7d1da757b0d1eb
GlobalHRIS <123946160+GlobalHRIS@users.noreply.github.com>
2023-03-16 22:36:47 +0000

    Update csvfile.py

--- csvfile.py
+++ csvfile.py
@@ -1,13 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-def text_to_csv(file):
-    data = []
-    with open(file) as f:
-        for line in f.readlines():
-            data.append(line.strip().split())
-    df = pd.DataFrame(data)
-    df.to_csv('output.csv', index=False)
+
 
 st.title('Text to CSV Converter')
 file = st.file_uploader("Upload a text file", type=["txt"])
